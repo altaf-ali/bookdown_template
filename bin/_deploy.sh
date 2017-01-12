0311d07940072a70beb4f06074b90a674eef3a5e
Altaf Ali <altaf@fastmail.com>
2017-01-12 18:40:09 +0000

    removed unnecessary copy command

--- bin/_deploy.sh
+++ bin/_deploy.sh
@@ -9,9 +9,10 @@ git config --global user.email "altaf@fastmail.com"
 git config --global user.name "Altaf Ali"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+
 cd book-output
 cp -r ../_book/* ./
-cp -r ../data ./
+
 git add --all *
 git commit -m"Update the book" || true
 git push origin gh-pages
